c3a7a6a7e04a618f12550fd2a8b5b67f6831ca40
Haobo Yuan <haoboyuan@ucmerced.edu>
2026-01-23 13:43:30 -0800

    fix the Root-Is-Purelib issue

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
-try:
-    from wheel.bdist_wheel import bdist_wheel
-    class bdist_wheel_abi3(bdist_wheel):
-        def finalize_options(self):
-            super().finalize_options()
-            self.root_is_pure = False
-            self.python_tag = "py3"
-except ImportError:
-    bdist_wheel_abi3 = None
+from setuptools.dist import Distribution
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(foo):
+        return True
 
 setup(
-    cmdclass={"bdist_wheel": bdist_wheel_abi3} if bdist_wheel_abi3 else {},
+    distclass=BinaryDistribution
 )
